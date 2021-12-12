a21a445fd6483649bb29600874ea5a5629b450e3
ljws93 <ljws93@naver.com>
2021-12-12 23:34:06 +0900

    make BottomBar(from Material) and make Fragment for it

--- app/src/main/java/com/example/mywiki_interviewtest/UI/MainActivity.kt
+++ app/src/main/java/com/example/mywiki_interviewtest/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mywiki_interviewtest
+package com.example.mywiki_interviewtest.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mywiki_interviewtest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
