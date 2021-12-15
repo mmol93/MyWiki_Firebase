61261fceb2f465ab7469b3ef996201968095cbea
ljws93 <ljws93@naver.com>
2021-12-16 08:26:56 +0900

    initial commit for DetailActivity

---
+++ app/src/main/java/com/example/mywiki_interviewtest/UI/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mywiki_interviewtest.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mywiki_interviewtest.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
